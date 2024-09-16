84f90a8683b508c5c3aecec7db05df03b4eb9458
Thallys_San <ricardo.viasolo142@gmail.com>
2024-09-16 14:42:08 -0300

    Lista 4 C++

---
+++ C++ 4/Atv2 - 4 c++.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int calculadora,num1, num2, result;
+    
+    cout << ""
+    cout << "digite qual operacao voce deseja\n";
+    cout << "(1) para adicao\n(2) para adicao\n(3) para multiplicacao\n(4) para divisao\ndigite qual ira deseja: ";
+    cin >> calculadora;
+    
+    switch (calculadora) {
+  case 1:
+    result = num1 + num2;
+    cout << "O resultado da soma e: "<< calculadora;
+    break;
+  case 2:
+    result = num1 - num2;
+    cout << "O resultado da subtracao e: "<< calculadora;
+    break;
+  case 3:
+  result = num1 * num2;
+    cout << "O resultado da multiplicacao e: "<< calculadora;
+    break;
+  case 4:
+  result = num1 / num2;
+    cout << "O resultado da divisao e: "<< calculadora;
+    break;
+}
+
+    return 0;
+}
