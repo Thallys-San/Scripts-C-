db7dc3e6e38a5475403fc99fd455916a8db1ac66
Thallys_San <ricardo.viasolo142@gmail.com>
2024-09-16 14:23:17 -0300

    Lista 1 C++
    exercicios senai c++

---
+++ C ++ 1/Atv - D c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main(){
+     string nome;
+     cout << "Digite um nome ";
+     cin >> nome;
+     
+     cout << "O nome digitado foi : " << nome;
+     cin >> nome;
+     return 0;
+}
+
