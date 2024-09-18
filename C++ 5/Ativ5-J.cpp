8d09948e399adb72a5d8680b8f02e4466588b600
Ryan3103 <ryangabrielsilvag052@gmail.com>
2024-09-18 13:45:40 -0300

    Add files via upload

---
+++ C++ 5/Ativ5-J.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int soma = 0;
+	for (int i = 0; i <= 100; i+=2){
+		cout << i << endl;
+		soma = soma + i;
+		}
+		cout << "O valor da soma dos numeros pares e: " <<soma;
+		
+		}
