5ea2b6355f1885d45e96bdaa4b734899024a51cc
Ryan3103 <ryangabrielsilvag052@gmail.com>
2024-09-18 13:47:41 -0300

    Ativ5-A.cpp

--- C++ 5/Ativ5-A.cpp
+++ C++ 5/Ativ5-A.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     int i, num, x;
-    cout << "Escolha uma opcao de idioma: ";
+    cout << "Os numeros pares de 1 a 100 sao: ";
     for (i=1; i<=x2; i++) {
         num= i%2;
         if (num%2 ==0)
